93d6adfe763dd3823d5eab8030a1aed60b6ba80b
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2023-04-07 11:23:31 -0300

    refactor: test in commit

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { Task, TaskSchema } from './schema/task.schema';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
     MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
   ],
   controllers: [AppController],
