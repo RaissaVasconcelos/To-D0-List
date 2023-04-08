43e087f84ac2be7c12c124bd851aa022cbb7839c
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2023-04-08 16:41:41 -0300

    chore: connection in mongodb

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), TodoModule],
+  imports: [MongooseModule.forRoot('mongodb://mongodb:27017/todo'), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/todo/todo.controller.ts
+++ server/src/todo/todo.controller.ts
@@ -10,8 +10,7 @@ import {
 
 import { AppService } from './todo.service';
 
-// test de commit
-@Controller('/task')
+@Controller('task')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
